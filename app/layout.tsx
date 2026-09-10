610eb5ef290e0b726f712f38f6cf71be977f1e63
nayo-money <nayomoney491@gmail.com>
2026-09-10 14:57:10 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -61,6 +62,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="zh-Hant-TW">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2GRZCJBR7G"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2GRZCJBR7G');
+          `}
+        </Script>
         <Header />
         {children}
         <Footer />
